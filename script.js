ea278339f38af2f26c42a80cbaf4506c48e32fa7
Reabetswe <reabetswemakgabutla0@gmail.com>
2024-05-08 00:02:04 +0200

    Added Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let headingOne = "WE'RE";
+let headingTwo ="COMING";
+let headingThree = "SOON";
+paragraph.textContent = "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.";
+let headingContainer = document.querySelector("#head");
+headingContainer.innerHTML = headingOne;
