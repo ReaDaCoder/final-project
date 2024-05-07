4b96c754473a25197416a889293b4dad1f4fb4b7
Reabetswe <reabetswemakgabutla0@gmail.com>
2024-05-08 00:50:59 +0200

    replaced query selector

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let headingOne = "WE'RE";
 let headingTwo ="COMING";
 let headingThree = "SOON";
 paragraph.textContent = "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.";
-let headingContainer = document.querySelector("#head");
+document.querySelector("#head").innerHTML= headingOne;
 headingContainer.innerHTML = headingOne;
