f1677a13256a5a80c4a937e4686b2f0223021e39
Reabetswe <reabetswemakgabutla0@gmail.com>
2024-05-08 11:15:03 +0200

    Added javascript content

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ let headingThree = "SOON";
 paragraph.textContent = "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.";
 document.querySelector("#head").innerHTML= headingOne;
 headingContainer.innerHTML = headingOne;
+document.querySelector("#heading").innerHTML= headingTwo;
+document.querySelector("#headingTwo").innerHTML= headingThree;
